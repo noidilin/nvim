5657a8f32f5172c2688db45b687d5e27efe3e95c
noidilin <linganinja.0120@gmail.com>
2025-02-13 23:22:02 +0800

    feat(plugin): add leetcode.nvim plugin

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -4,7 +4,7 @@ return {
 	"kawre/leetcode.nvim",
 	lazy = leet_arg ~= vim.fn.argv()[1],
 	-- cmd = "Leet",
-	build = ":TSUpdate html",
+	build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
 	dependencies = {
 		"nvim-telescope/telescope.nvim",
 		"nvim-lua/plenary.nvim", -- required by telescope
@@ -12,7 +12,6 @@ return {
 
 		-- optional
 		"nvim-treesitter/nvim-treesitter",
-		-- "rcarriga/nvim-notify",
 		"nvim-tree/nvim-web-devicons",
 	},
 	opts = {
