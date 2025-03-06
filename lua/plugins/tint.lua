ebc5bdfcc6922160f4eebf2c235b34cc9a87dfe2
noidilin <linganinja.0120@gmail.com>
2025-03-06 17:48:53 +0800

    refactor(plugin): optimize plugin startup process for tint.nvim

--- lua/plugins/tint.lua
+++ lua/plugins/tint.lua
@@ -1,14 +1,9 @@
 return {
 	"levouh/tint.nvim",
-	opts = {
-		-- set tint and saturation with transforms in config field.
-		-- tint = -30, -- Darken colors, use a positive value to brighten
-		-- saturation = 0.1, -- Saturation to preserve
-		tint_background_colors = false, -- Tint background portions of highlight groups
-		-- highlight_ignore_patterns = { "WinSeparator", "Status.*" },  -- Highlight group patterns to ignore, see `string.find`
-	},
-	config = function()
-		require("tint").setup({
+	event = "VeryLazy",
+	opts = function()
+		local tint = require("tint")
+		tint.setup({
 			-- ref: https://github.com/levouh/tint.nvim/blob/master/DOC.md#transforms-tint_with_threshold
 			transforms = {
 				-- WARN: bad practice to hardcode a color value like #191919
@@ -16,5 +11,9 @@ return {
 				require("tint.transforms").saturate(0.4),
 			},
 		})
+		return {
+			-- tint and saturation is already set with transforms in setup().
+			tint_background_colors = false, -- Tint background portions of highlight groups
+		}
 	end,
 }
