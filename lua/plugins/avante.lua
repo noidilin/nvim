48cf3e1c127ec8e36841910859397f03a9cedc52
noidilin <linganinja.0120@gmail.com>
2025-03-09 00:22:46 +0800

    feat(plugin): add support to build avante.nvim with rust

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,13 +1,10 @@
 return {
 	"yetone/avante.nvim",
 	cmd = { "AvanteAsk", "AvanteChat", "AvanteEdit", "AvanteToggle" },
-	-- event = "VeryLazy",
-	-- lazy = false,
 	version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
 	opts = {
 		---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-		provider = "claude", -- Recommend using Claude
-		-- provider = "openai",
+		provider = "claude", -- recommend using Claude
 		claude = {
 			endpoint = "https://api.anthropic.com",
 			model = "claude-3-5-sonnet-20241022",
@@ -24,7 +21,7 @@ return {
 	},
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 	-- build = "make",
-	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false", -- for windows
 	dependencies = {
 		"stevearc/dressing.nvim",
 		"nvim-lua/plenary.nvim",
