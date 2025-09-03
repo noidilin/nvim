18809eef3209bdb904c73bddf7a789fb08120924
noidilin <linganinja.0120@gmail.com>
2025-09-03 19:36:31 +0800

    refactor(plugin): disable img-clip, avante

--- lua/plugins/__img-clip.lua
+++ lua/plugins/__img-clip.lua
@@ -1,6 +1,8 @@
--- ref - https://github.com/linkarzu/dotfiles-latest
--- ISSUE: [content is not an image #13](https://github.com/HakonHarnes/img-clip.nvim/issues/13)
--- It might has some issue with powershell command. Try inspect with the command ":ImgClipDebug"
+-- stylua: ignore
+if true then return {} end
+
+-- ISSUE: nushell in windows device
+-- [vim shell set to nushell caused windows device couldn't get clipboard image · Issue #103](https://github.com/hakonharnes/img-clip.nvim/issues/103)
 
 return {
 	"HakonHarnes/img-clip.nvim",
@@ -9,19 +11,17 @@ return {
 		default = {
 			-- file and directory options
 			dir_path = "./extra/asset", ---@type string | fun(): string
-			extension = "avif", ---@type string | fun(): string
+			-- extension = "avif", ---@type string | fun(): string
+			extension = "jpg", ---@type string | fun(): string
 			file_name = "r-%Y%m%d%H%M%S", ---@type string | fun(): string
-			-- disable absolute path to switch device easily
 			use_absolute_path = false, ---@type boolean | fun(): boolean
 			-- if set to false, image will be created in cwd
 			relative_to_current_file = false, ---@type boolean | fun(): boolean
 
-			-- template options
 			insert_mode_after_paste = false, ---@type boolean | fun(): boolean
 			prompt_for_file_name = false, ---@type boolean | fun(): boolean
 
-			-- image options
-			process_cmd = "convert - -quality 75 avif:-", ---@type string | fun(): string
+			process_cmd = "convert - -quality 75 jpg:-", ---@type string | fun(): string
 			-- process_cmd = "convert - -quality 75 -resize 50% jpg:-", ---@type string | fun(): string
 			copy_images = true, ---@type boolean | fun(): boolean
 		},
@@ -42,6 +42,5 @@ return {
 			},
 		},
 	},
-	-- { "<leader>np", "<cmd>PasteImage<cr>", desc = "paste img", icon = { icon = "" }, },
 	keys = { { "<leader>np", "<cmd>PasteImage<cr>", desc = "paste img" } },
 }
