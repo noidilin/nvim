26986908670f57fbddab56344e1be0f64c983b23
noidilin <linganinja.0120@gmail.com>
2025-09-06 20:56:01 +0800

    feat(plugin): add minty and showkeys plugin
    - minty: color tools for adjusting shade and hue
    - showkeys: screen cast key stroke on the screen

---
+++ lua/plugins/nvzone.lua
@@ -0,0 +1,16 @@
+return {
+	{ "nvzone/volt", lazy = true },
+	{
+		"nvzone/minty",
+		cmd = { "Shades", "Huefy" },
+	},
+	{
+		"nvzone/showkeys",
+		cmd = "ShowkeysToggle",
+		opts = {
+			timeout = 2,
+			maxkeys = 5,
+			-- show_count = true,
+		},
+	},
+}
