f3a493623e75739de2a29575f4e63ffd8d9695db
noidilin <linganinja.0120@gmail.com>
2025-09-10 18:31:40 +0800

    feat(plugin): add treesitter split join (treesj) plugin

---
+++ lua/plugins/editing.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"nvim-mini/mini.operators",
+		event = "VeryLazy",
+		version = false,
+		-- No need to copy this inside `setup()`. Will be used automatically.
+		opts = {
+			-- Evaluate text and replace with output
+			evaluate = { prefix = "ge=", func = nil },
+			-- Exchange text regions
+			exchange = { prefix = "gex", reindent_linewise = true },
+			-- Multiply (duplicate) text
+			multiply = { prefix = "gem", func = nil },
+			-- Replace text with register
+			replace = { prefix = "ger", reindent_linewise = true },
+			-- Sort text
+			sort = { prefix = "ges", func = nil },
+		},
+	},
+	{
+		"Wansmer/treesj",
+		keys = { { "get", "<CMD>TSJToggle<CR>", desc = "Toggle Inline/Block" } },
+		dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+		opts = {
+			use_default_keymaps = false,
+		},
+	},
+}
