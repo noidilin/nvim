9b63d7fd6dd453c0fcc41d5102bf03838cc5b803
noidilin <linganinja.0120@gmail.com>
2025-09-14 13:22:08 +0800

    fix(treesitter): separate jsx query from ecma

---
+++ after/queries/jsx/textobjects.scm
@@ -0,0 +1,12 @@
+; JSX/TSX attributes
+(jsx_attribute
+  (property_identifier) @attribute.lhs
+  "="
+  [
+    (string)
+    (jsx_expression)
+  ] @attribute.rhs @attribute.inner) @attribute.outer
+
+; Self-closing JSX/TSX attribute (boolean attributes)
+(jsx_attribute
+  (property_identifier) @attribute.inner) @attribute.outer
