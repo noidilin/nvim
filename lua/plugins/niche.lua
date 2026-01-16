722f6d84b31872d813d120b8ee64025eb8fb3e26
noidilin <linganinja.0120@gmail.com>
2026-01-16 12:28:42 +0800

    feat(plugin): add to-do list plugin

--- lua/plugins/niche.lua
+++ lua/plugins/niche.lua
@@ -1,4 +1,12 @@
 return {
 	{ "nvzone/showkeys", cmd = "ShowkeysToggle", opts = { maxkeys = 5 } },
 	-- { "nvzone/minty", cmd = { "Shades", "Huefy" }, dependencies = { "nvzone/volt" } },
+	{
+		"atiladefreitas/dooing",
+		config = {
+			notes = { icon = "" },
+			due_notifications = { on_startup = true },
+			keymaps = { create_nested_task = "n" },
+		},
+	},
 }
