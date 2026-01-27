9f329849ff5120829a41f485a0893f754d69c27e
noidilin <linganinja.0120@gmail.com>
2026-01-27 14:00:22 +0800

    feat(plugin): add wakatimer to track coding time

--- lua/plugins/niche.lua
+++ lua/plugins/niche.lua
@@ -1,6 +1,7 @@
 return {
 	{ "nvzone/showkeys", cmd = "ShowkeysToggle", opts = { maxkeys = 5 } },
 	-- { "nvzone/minty", cmd = { "Shades", "Huefy" }, dependencies = { "nvzone/volt" } },
+	{ "wakatime/vim-wakatime", lazy = false },
 	{
 		"atiladefreitas/dooing",
 		config = {
