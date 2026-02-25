18e23e329d51eea5f125bb1cf9846fe6448a6f76
noidilin <linganinja.0120@gmail.com>
2026-02-25 13:26:54 +0800

    feat(plugins): add gh from snacks nvim plugin

--- lua/plugins/_core.lua
+++ lua/plugins/_core.lua
@@ -11,7 +11,21 @@ return {
 	{
 		"folke/snacks.nvim",
 		opts = {
+			gh = {
+				-- your gh configuration comes here
+				-- or leave it empty to use the default settings
+			},
 			picker = {
+				sources = {
+					gh_issue = {
+						-- your gh_issue picker configuration comes here
+						-- or leave it empty to use the default settings
+					},
+					gh_pr = {
+						-- your gh_pr picker configuration comes here
+						-- or leave it empty to use the default settings
+					},
+				},
 				win = {
 					input = {
 						keys = {
@@ -52,6 +66,36 @@ return {
 							},
 						}
 				end,
+			},
+		},
+		keys = {
+			{
+				"<leader>gi",
+				function()
+					Snacks.picker.gh_issue()
+				end,
+				desc = "GitHub Issues (open)",
+			},
+			{
+				"<leader>gI",
+				function()
+					Snacks.picker.gh_issue({ state = "all" })
+				end,
+				desc = "GitHub Issues (all)",
+			},
+			{
+				"<leader>gp",
+				function()
+					Snacks.picker.gh_pr()
+				end,
+				desc = "GitHub Pull Requests (open)",
+			},
+			{
+				"<leader>gP",
+				function()
+					Snacks.picker.gh_pr({ state = "all" })
+				end,
+				desc = "GitHub Pull Requests (all)",
 			},
 		},
 	},
