aec99e6723cdc80584f53e876c470eb5b4dbcf48
noidilin <linganinja.0120@gmail.com>
2026-04-11 18:41:45 +0800

    feat(lang): add support to detail rust error message tool rustowl

--- lua/plugins/_lang.lua
+++ lua/plugins/_lang.lua
@@ -4,6 +4,27 @@ return {
 		opts = { ensure_installed = { 'powershell' } },
 	},
 	{
+		'cordx56/rustowl',
+		version = '*', -- Latest stable version
+		build = 'cargo binstall rustowl', -- cargo-binstall is needed
+		lazy = false, -- This plugin is already lazy
+		opts = function(_, opts)
+			local p = require('theme.lua.lush_theme._primitive').hex
+			opts = {
+				colors = {
+					lifetime = p.green00,
+					imm_borrow = p.blue00,
+					mut_borrow = p.magenta00,
+					move = p.orange00,
+					call = p.yellow00,
+					outlive = p.red00,
+				},
+			}
+
+			return opts
+		end,
+	},
+	{
 		'mason-org/mason.nvim',
 		opts = {
 			ensure_installed = {
